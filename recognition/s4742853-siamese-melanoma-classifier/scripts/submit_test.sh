dbef16f2f95325c6f0d6642371d5a2e1efc792e2
Atticus Johnston <atticusjohnston04@gmail.com>
2025-10-26 07:03:29 +1000

    tweaking test submission.

--- recognition/s4742853-siamese-melanoma-classifier/scripts/submit_test.sh
+++ recognition/s4742853-siamese-melanoma-classifier/scripts/submit_test.sh
@@ -21,9 +21,9 @@ python train.py \
     --train-img-dir data/cleaned/train_images_224 \
     --val-csv data/cleaned/validation_pairs.csv \
     --val-img-dir data/cleaned/validation_images_224 \
-    --batch-size 128 \
+    --batch-size 256 \
     --epochs 1 \
-    --lr 1e-4 \
+    --lr 1e-3 \
     --save-dir models \
     --model custom \
     --log-file logs/train_$SLURM_JOB_ID.log
