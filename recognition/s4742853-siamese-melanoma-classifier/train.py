a26118e789d461bec41eac246d7f4ca1a2bca7cb
Atticus Johnston <atticusjohnston04@gmail.com>
2025-10-25 19:52:21 +1000

    Simple plotter and models save path.

--- recognition/s4742853-siamese-melanoma-classifier/train.py
+++ recognition/s4742853-siamese-melanoma-classifier/train.py
@@ -5,6 +5,29 @@ from tqdm import tqdm
 from dataset import SiameseMelanomaClassifierDataset
 from modules import SiameseNetwork
 from torch.utils.data import DataLoader
+import time
+import matplotlib.pyplot as plt
+import os
+
+
+class Plotter:
+    def __init__(self):
+        self.train_losses = []
+        self.val_losses = []
+
+    def add(self, train_loss, val_loss):
+        self.train_losses.append(train_loss)
+        self.val_losses.append(val_loss)
+
+    def plot(self, save_path):
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.train_losses, label='Train Loss')
+        plt.plot(self.val_losses, label='Val Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.savefig(save_path)
+        plt.close()
 
 
 class Trainer:
@@ -22,6 +45,7 @@ class Trainer:
         self.criterion = nn.BCELoss()
         self.optimiser = optim.Adam(network.parameters(), lr=lr)
         self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimiser, gamma=0.99)
+        self.plotter = Plotter()
 
     def train_epoch(self, pbar):
         self.network.train()
@@ -67,11 +91,22 @@ class Trainer:
         for epoch in pbar:
             train_loss = self.train_epoch(pbar)
             val_loss = self.validate(pbar)
+            self.plotter.add(train_loss, val_loss)
             self.scheduler.step()
+
+        timestamp = int(time.time())
+        os.makedirs('models', exist_ok=True)
+        model_path = f'models/siamese_melanoma_classifier_{timestamp}.pt'
+        torch.save(self.network.state_dict(), model_path)
+        self.plotter.plot(f'models/siamese_melanoma_classifier_{timestamp}_loss.png')
 
 
 if __name__ == "__main__":
-    device = torch.device("mps" if torch.mps.is_available() else "cpu")
+    device = torch.device(
+        "mps" if torch.mps.is_available()
+        else "cuda" if torch.cuda.is_available()
+        else "cpu"
+    )
 
     train_dataset = SiameseMelanomaClassifierDataset('data/cleaned/train_pairs.csv',
                                                      'data/cleaned/train_images',
