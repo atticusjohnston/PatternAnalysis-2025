e5cfc8e430db26a636738a5cd3c99f4a6027ceea
Atticus Johnston <atticusjohnston04@gmail.com>
2025-10-26 13:12:27 +1000

    Info logs

--- recognition/s4742853-siamese-melanoma-classifier/train.py
+++ recognition/s4742853-siamese-melanoma-classifier/train.py
@@ -22,7 +22,7 @@ def setup_logging(log_file=None):
         handlers.append(logging.FileHandler(log_file))
 
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=handlers
     )
