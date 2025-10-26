adbf69f0a629c8707d1c858662d287b230ac3182
Atticus Johnston <atticusjohnston04@gmail.com>
2025-10-26 11:53:56 +1000

    Trying without alpha.

--- recognition/s4742853-siamese-melanoma-classifier/modules.py
+++ recognition/s4742853-siamese-melanoma-classifier/modules.py
@@ -96,6 +96,7 @@ class PretrainedSiameseNetwork(nn.Module):
             logger.debug(f"alpha: min={self.alpha.min():.3f}, max={self.alpha.max():.3f}")
 
         distance = torch.abs(h1 - h2)
-        weighted_distance = torch.sum(self.alpha * distance, dim=1)
+        # weighted_distance = torch.sum(self.alpha * distance, dim=1)
+        weighted_distance = torch.sum(distance, dim=1)
         p = torch.sigmoid(weighted_distance)
         return p
